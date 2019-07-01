92dd9bc032102cfbdc40cdbf8512c865a65e0470
liuyao <liuyao@ixianlai.com>
2019-07-01 17:33:04 +0800

    update two sum

---
+++ leetcode/two_sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int *twoSum(int *nums, int numSize, int target)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
--- leetcode/two_sum.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# __author__ = "YaoYao"
-# Date: 2019-07-01
-
